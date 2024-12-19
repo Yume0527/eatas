f907cb7935dec47daa90da31232a4b946bbc38b6
Yume0527 <s23m1067@bene.fit.ac.jp>
2024-12-19 14:35:59 +0900

    エラー修正

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -5,13 +5,13 @@
             <div class="flex">
                 <!-- Logo -->
                 <div class="shrink-0 flex items-center">
-                    <a href="{{ route('Home') }}">
+                    <a href="{{ route('home') }}">
                         <x-application-logo class="block h-9 w-auto fill-current text-gray-800 dark:text-gray-200" />
                     </a>
                 </div>
                 <!-- Navigation Links -->
                 <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
-                    <x-nav-link :href="route('Home')" :active="request()->routeIs('Home')">
+                    <x-nav-link :href="route('home')" :active="request()->routeIs('Home')">
                         {{ __('ホーム') }}
                     </x-nav-link>
                     <x-nav-link :href="route('record.input')" :active="request()->routeIs('record.input')">
@@ -72,7 +72,7 @@
     <!-- Responsive Navigation Menu -->
     <div :class="{'block': open, 'hidden': ! open}" class="hidden sm:hidden">
         <div class="pt-2 pb-3 space-y-1">
-            <x-responsive-nav-link :href="route('Home')" :active="request()->routeIs('Home')">
+            <x-responsive-nav-link :href="route('home')" :active="request()->routeIs('Home')">
                 {{ __('ホーム') }}
             </x-responsive-nav-link>
             <x-responsive-nav-link :href="route('record.input')" :active="request()->routeIs('record.input')">
